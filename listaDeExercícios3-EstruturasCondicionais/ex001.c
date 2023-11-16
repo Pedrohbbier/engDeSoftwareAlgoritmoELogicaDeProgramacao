75ec4202a162c840e83de448a7774ef1902b75cb
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2023-11-15 23:40:05 -0300

    Atualizando para o modelo do visual studio code

--- listaDeExercícios3-EstruturasCondicionais/ex001.c
+++ listaDeExercícios3-EstruturasCondicionais/ex001.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-main(){
+int main(){
 	float MAIOR , INTER , MENOR ;
 	printf("Digite o primeiro numero: ");
 	scanf("%f" , &MAIOR);
--- listaDeExercícios3-EstruturasCondicionais/ex002.c
+++ listaDeExercícios3-EstruturasCondicionais/ex002.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-main(){
+int main(){
 	int n1;
 	printf("Digite o numero: ");
 	scanf("%d" , &n1);
